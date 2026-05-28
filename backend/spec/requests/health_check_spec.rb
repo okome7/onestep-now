4f8a7894f682762dfc6e03f2cbe36156730e1521
okome7 <jiumijingyouyi@gmail.com>
2026-05-28 20:46:44 +0900

    fix: RuboCopの警告（末尾の空白と空行）を修正

--- backend/spec/requests/health_check_spec.rb
+++ backend/spec/requests/health_check_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "HealthCheck", type: :request do
   describe "GET /up" do
     it "returns http success" do
-      get rails_health_check_path 
+      get rails_health_check_path
       expect(response).to have_http_status(:success)
     end
   end
