9af2e4aa09ef363004b8f03271f15ebd9244df48
okome7 <jiumijingyouyi@gmail.com>
2026-05-30 13:39:29 +0900

    feat: Userモデルにパスワード暗号化とバリデーションを追加

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  has_many :tasks, dependent: :destroy
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
