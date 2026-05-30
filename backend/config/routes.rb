445525b4cde3ccb602a0c3bccfd38d154580aa61
okome7 <jiumijingyouyi@gmail.com>
2026-05-30 19:31:36 +0900

    chore: 動作確認用にルートパス(root)のレスポンスを追加

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root to: ->(_) { [200, { "Content-Type" => "application/json" }, [{ status: "Running" }.to_json]] }
+  
   get "up" => "rails/health#show", as: :rails_health_check
-
   # 新規登録
   post "signup", to: "registrations#create"
 end
