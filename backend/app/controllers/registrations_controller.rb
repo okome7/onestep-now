fec74fcdf76c9f9ccf783cd2bf951cf455a62e6b
okome7 <jiumijingyouyi@gmail.com>
2026-06-06 10:45:59 +0900

    feat: ユーザー登録用のAPIエンドポイントを実装

--- backend/app/controllers/registrations_controller.rb
+++ backend/app/controllers/registrations_controller.rb
@@ -1,11 +1,12 @@
 class RegistrationsController < ApplicationController
-  # 新規登録アクション
+  # skip_before_action :verify_authenticity_token # APIとして使うために必要
+
   def create
     user = User.new(user_params)
     if user.save
-      render json: { user: { id: user.id, name: user.name, email: user.email } }, status: :created
+      render json: { status: 'success', data: user }, status: :created
     else
-      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+      render json: { status: 'error', errors: user.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
