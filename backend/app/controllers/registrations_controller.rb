d93ced61a8cab0f3c7f50782b24073a54e10c378
okome7 <jiumijingyouyi@gmail.com>
2026-06-06 10:53:09 +0900

    style: registrations_controllerのクォーテーションのスタイルを修正

--- backend/app/controllers/registrations_controller.rb
+++ backend/app/controllers/registrations_controller.rb
@@ -4,9 +4,9 @@ class RegistrationsController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      render json: { status: 'success', data: user }, status: :created
+      render json: { status: "success", data: user }, status: :created
     else
-      render json: { status: 'error', errors: user.errors.full_messages }, status: :unprocessable_entity
+      render json: { status: "error", errors: user.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
